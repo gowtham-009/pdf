64890cb526cfd3d65b8401fb5358b63a29eb7ef9
gowtham <gowtham05092000@gmail.com>
2024-04-30 13:41:12 +0530

    first commit

---
+++ components/PdfViewer.vue
@@ -0,0 +1,18 @@
+<!-- components/PdfViewer.vue -->
+<template>
+    <div>
+      <iframe :src="pdfSrc" width="100%" height="600"></iframe>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      pdfSrc: {
+        type: String,
+        required: true
+      }
+    }
+  };
+  </script>
+  
---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<!-- pages/MyPdfPage.vue -->
+<template>
+  <div>
+    <h1 style="text-align: center;">pdf viewer</h1>
+    <PdfViewer :pdfSrc="pdfUrl" />
+  </div>
+</template>
+
+<script>
+import PdfViewer from '~/components/PdfViewer.vue';
+
+export default {
+  components: {
+    PdfViewer
+  },
+  data() {
+    return {
+      pdfUrl: '/gowtham.pdf'
+    };
+  }
+};
+</script>
